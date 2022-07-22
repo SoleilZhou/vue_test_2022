edbead3d35b1b3fd9fdbd543cb7b3201f2699dd5
zhouxinrui <1352492197@qq.com>
2022-07-22 14:11:46 +0800

    update

--- 19_src_配置代理服务器/main.js
+++ 19_src_配置代理服务器/main.js
@@ -1,11 +1,8 @@
 //该文件是整个项目的文件入口
 import Vue from 'vue'         //引入Vue
 import App from './App.vue'   //引入App组件，它是所有组件的父组件
-import vueResource from 'vue-resource'  //引入插件
 
 Vue.config.productionTip = false
-
-Vue.use(vueResource)  //使用插件
 
 new Vue({
     el:'#app',
