f2a8d647353ae8d24c22133db197b7688239a4fe
zhouxinrui <1352492197@qq.com>
2022-07-25 22:01:17 +0800

    update

--- 27_src_路由的基本使用/App.vue
+++ 27_src_路由的基本使用/App.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <div class="row">
-      <Banner/>
+      <div class="col-xs-offset-2 col-xs-8">
+        <div class="page-header"><h2>Vue Router Demo</h2></div>
+      </div>
     </div>
     <div class="row">
       <div class="col-xs-2 col-xs-offset-2">
         <div class="list-group">
+          <!-- 原始html中使用a标签实现页面的跳转 -->
+          <!-- <a class="list-group-item active" href="./about.html">About</a>
+          <a class="list-group-item" href="./home.html">Home</a> -->
+
           <!-- Vue借助router-link标签实现路由的切换 -->
           <router-link class="list-group-item" active-class="active" to="/about">About</router-link>
           <router-link class="list-group-item" active-class="active" to="/home">Home</router-link>
@@ -24,9 +30,7 @@
 </template>
 
 <script>
-  import Banner from './components/Banner';
   export default {
     name: "App",
-    components: { Banner }
 }
 </script>
